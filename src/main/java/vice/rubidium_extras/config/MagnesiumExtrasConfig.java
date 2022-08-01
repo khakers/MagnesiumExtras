dc9aa2fe66887c1d3e5a3e31ead1a44467a9a368
Khakers <22665282+khakers@users.noreply.github.com>
2022-07-31 21:19:48 -0700

    remove zoom config, bump version

--- src/main/java/vice/rubidium_extras/config/MagnesiumExtrasConfig.java
+++ src/main/java/vice/rubidium_extras/config/MagnesiumExtrasConfig.java
@@ -6,12 +6,12 @@ import me.jellysquid.mods.sodium.client.gui.options.TextProvider;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TextComponent;
 import net.minecraftforge.common.ForgeConfigSpec;
+
 import java.nio.file.Path;
 
 import net.minecraftforge.common.ForgeConfigSpec.ConfigValue;
 
-public class MagnesiumExtrasConfig
-{
+public class MagnesiumExtrasConfig {
     public static ForgeConfigSpec ConfigSpec;
 
     public static ConfigValue<String> fadeInQuality;
@@ -19,7 +19,6 @@ public class MagnesiumExtrasConfig
     public static ConfigValue<String> fpsCounterMode;
     public static ConfigValue<Integer> fpsCounterPosition;
     public static ForgeConfigSpec.ConfigValue<Integer> cloudHeight;
-
 
     public static ConfigValue<Integer> maxTileEntityRenderDistanceSquare;
     public static ConfigValue<Integer> maxTileEntityRenderDistanceY;
@@ -32,18 +31,7 @@ public class MagnesiumExtrasConfig
 
     public static ConfigValue<Boolean> hideJEI;
 
-    // Ok Zoomer
-    public static ZoomValues zoomValues = new ZoomValues();
-    public static ConfigValue<Boolean> lowerZoomSensitivity;
-    public static ConfigValue<String> zoomTransition;
-    public static ConfigValue<String> zoomMode;
-    public static ConfigValue<String> cinematicCameraMode;
-    public static ConfigValue<Boolean> zoomScrolling;
-    public static ConfigValue<Boolean> zoomOverlay;
-
-
     public static ForgeConfigSpec.EnumValue<FullscreenMode> fullScreenMode;
-
 
     // Total Darkness
     public static double darkNetherFogEffective;
@@ -63,15 +51,12 @@ public class MagnesiumExtrasConfig
     public static ForgeConfigSpec.BooleanValue darkDefault;
     public static ForgeConfigSpec.BooleanValue darkNether;
 
-
-
-    static
-    {
+    static {
         var builder = new ConfigBuilder("Dynamic Lights Settings");
 
         builder.Block("Misc", b -> {
             cloudHeight = b.define("Cloud Height [Raw, Default 256]", 256);
-            fadeInQuality =  b.define("Chunk Fade In Quality (OFF, FAST, FANCY)", "FANCY");
+            fadeInQuality = b.define("Chunk Fade In Quality (OFF, FAST, FANCY)", "FANCY");
             fog = b.define("Render Fog", true);
             hideJEI = b.define("Hide JEI Until Searching", true);
             fullScreenMode = b.defineEnum("Use Borderless Fullscreen", FullscreenMode.FULLSCREEN);
@@ -90,16 +75,6 @@ public class MagnesiumExtrasConfig
 
             maxEntityRenderDistanceSquare = b.define("(Entity) Max Horizontal Render Distance [Squared, Default 64^2]", 4096);
             maxEntityRenderDistanceY = b.define("(Entity) Max Vertical Render Distance [Raw, Default 32]", 32);
-        });
-
-        builder.Block("Zoom", b -> {
-            lowerZoomSensitivity = b.define("Lower Zoom Sensitivity", true);
-            zoomScrolling = b.define("Zoom Scrolling Enabled", true);
-            zoomTransition = b.define("Zoom Transition Mode (OFF, LINEAR, SMOOTH)", ZoomTransitionOptions.SMOOTH.toString());
-            zoomMode = b.define("Zoom Transition Mode (TOGGLE, HOLD, PERSISTENT)", ZoomModes.HOLD.toString());
-            cinematicCameraMode = b.define("Cinematic Camera Mode (OFF, VANILLA, MULTIPLIED)", CinematicCameraOptions.OFF.toString());
-            zoomOverlay = b.define("Zoom Overlay?", true);
-            //zoomValues = b.define("Zoom Advanced Values", new ZoomValues());
         });
 
         builder.Block("True Darkness", b -> {
@@ -134,9 +109,7 @@ public class MagnesiumExtrasConfig
         ConfigSpec.setConfig(configData);
     }
 
-
-    public static enum Complexity implements TextProvider
-    {
+    public static enum Complexity implements TextProvider {
         OFF("Off"),
         SIMPLE("Simple"),
         ADVANCED("Advanced");
@@ -152,8 +125,7 @@ public class MagnesiumExtrasConfig
         }
     }
 
-    public static enum Quality implements TextProvider
-    {
+    public static enum Quality implements TextProvider {
         OFF("Off"),
         FAST("Fast"),
         FANCY("Fancy");
@@ -172,7 +144,7 @@ public class MagnesiumExtrasConfig
 
     public enum DarknessOption {
         PITCH_BLACK(0f),
-        REALLY_DARK (0.04f),
+        REALLY_DARK(0.04f),
         DARK(0.08f),
         DIM(0.12f);
 
@@ -189,56 +161,4 @@ public class MagnesiumExtrasConfig
         FULLSCREEN
     }
 
-    public enum ZoomTransitionOptions {
-        OFF,
-        SMOOTH
-    }
-
-    public enum ZoomModes {
-        HOLD,
-        TOGGLE,
-        PERSISTENT
-    }
-
-    public enum CinematicCameraOptions {
-        OFF,
-        VANILLA,
-        MULTIPLIED
-    }
-
-    public static class ZoomValues {
-        //@Setting.Constrain.Range(min = Double.MIN_NORMAL)
-        //@Setting(comment = "The divisor applied to the FOV when zooming.")
-        public double zoomDivisor = 4.0;
-
-        //@Setting.Constrain.Range(min = Double.MIN_NORMAL)
-        //@Setting(comment = "The minimum value that you can scroll down.")
-        public double minimumZoomDivisor = 1.0;
-
-        //@Setting.Constrain.Range(min = Double.MIN_NORMAL)
-        //@Setting(comment = "The maximum value that you can scroll up.")
-        public double maximumZoomDivisor = 50.0;
-
-        //@Setting.Constrain.Range(min = 0.0)
-        //@Setting(comment = "The number which is de/incremented by zoom scrolling. Used when the zoom divisor is above the starting point.")
-        public double scrollStep = 1.0;
-
-       //"The number which is de/incremented by zoom scrolling. Used when the zoom divisor is below the starting point.")
-        public double lesserScrollStep = 0.5;
-
-        //@Setting.Constrain.Range(min = Double.MIN_NORMAL)
-        //@Setting(comment = "The multiplier used for the multiplied cinematic camera.")
-        public double cinematicMultiplier = 4.0;
-
-        ////@Setting.Constrain.Range(min = Double.MIN_NORMAL, max = 1.0)
-        //@Setting(comment = "The multiplier used for smooth transitions.")
-        public double smoothMultiplier = 0.75;
-
-        //@Setting(comment = "The minimum value which the linear transition step can reach.")
-        public double minimumLinearStep = 0.125;
-
-        //@Setting.Constrain.Range(min = Double.MIN_NORMAL)
-       // @Setting(comment = "The maximum value which the linear transition step can reach.")
-        public double maximumLinearStep = 0.25;
-    }
 }
